c6eb4406d519b690c3cc420a40c77eef305d78bd
Lucie Philippon <julien.philippon@epitech.eu>
2021-08-01 15:42:33 +0200

    Fix compilation of tests for libecs and libgraphic

--- libecs/tests/basics/driver.cxx
+++ libecs/tests/basics/driver.cxx
@@ -2,32 +2,8 @@
 #include <sstream>
 #include <stdexcept>
 
-#include <libecs/version.hxx>
-#include <libecs/ecs.hxx>
+import pivot.ecs;
 
 int main ()
 {
-  using namespace std;
-  using namespace ecs;
-
-  // Basics.
-  //
-  {
-    ostringstream o;
-    say_hello (o, "World");
-    assert (o.str () == "Hello, World!\n");
-  }
-
-  // Empty name.
-  //
-  try
-  {
-    ostringstream o;
-    say_hello (o, "");
-    assert (false);
-  }
-  catch (const invalid_argument& e)
-  {
-    assert (e.what () == string ("empty name"));
-  }
 }
--- libgraphic/tests/basics/driver.cxx
+++ libgraphic/tests/basics/driver.cxx
@@ -2,32 +2,8 @@
 #include <sstream>
 #include <stdexcept>
 
-#include <libgraphic/version.hxx>
-#include <libgraphic/graphic.hxx>
+import pivot.graphic;
 
 int main ()
 {
-  using namespace std;
-  using namespace graphic;
-
-  // Basics.
-  //
-  {
-    ostringstream o;
-    say_hello (o, "World");
-    assert (o.str () == "Hello, World!\n");
-  }
-
-  // Empty name.
-  //
-  try
-  {
-    ostringstream o;
-    say_hello (o, "");
-    assert (false);
-  }
-  catch (const invalid_argument& e)
-  {
-    assert (e.what () == string ("empty name"));
-  }
 }

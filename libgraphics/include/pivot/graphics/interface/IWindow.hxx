4a44f0cee518720cbd7dc212fc16a49db920cab9
Zacharie Corniere <zacharie@corniere.me>
2021-08-07 09:56:54 +0200

    fix(IWindow): added get required extensions

--- libgraphics/include/pivot/graphics/interface/IWindow.hxx
+++ libgraphics/include/pivot/graphics/interface/IWindow.hxx
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <vector>
 #include <vulkan/vulkan_core.h>
+
 class IWindow
 {
 public:
     virtual ~IWindow() = 0;
     virtual void createSurface(const VkInstance &, VkSurfaceKHR *) = 0;
     virtual VkExtent2D getSize() = 0;
+
+    virtual std::vector<const char *> getRequiredExtensions() = 0;
 };

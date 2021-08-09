f8dfb18e74126350a71f5a069b2a9ed70551d9c0
Zacharie Corniere <zacharie@corniere.me>
2021-08-09 16:39:33 +0200

    fix(libgraphics): fixed vulkan initialization order

--- libgraphics/include/pivot/graphics/Swapchain.hxx
+++ libgraphics/include/pivot/graphics/Swapchain.hxx
@@ -6,12 +6,6 @@
 
 #include "pivot/graphics/DeletionQueue.hxx"
 
-struct CreationParameters {
-    VkPolygonMode polygonMode = VK_POLYGON_MODE_FILL;
-    VkSampleCountFlagBits msaaSample = VK_SAMPLE_COUNT_1_BIT;
-    VkCullModeFlags cullMode = VK_CULL_MODE_BACK_BIT;
-};
-
 class Swapchain
 {
 public:

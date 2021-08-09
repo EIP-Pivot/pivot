b121bfe493f990599a0ee9b823378803ec4a5bac
Zacharie Corniere <zacharie@corniere.me>
2021-08-09 14:23:49 +0200

    fix(VulkanApplication): give IWindow as arguments for init function

--- libgraphics/include/pivot/graphics/interface/IWindow.hxx
+++ libgraphics/include/pivot/graphics/interface/IWindow.hxx
@@ -6,7 +6,6 @@
 class IWindow
 {
 public:
-    virtual ~IWindow() = 0;
     virtual void createSurface(const VkInstance &, VkSurfaceKHR *) = 0;
     virtual const VkExtent2D &getSize() = 0;
     virtual std::vector<const char *> getWindowRequiredExtensions() = 0;

3859a77df9c4ded99e4fbe73bdbaafe0c4fafbad
M@x <maxence.pellerin@epitech.eu>
2021-09-09 14:47:28 +0200

    feat(ECS): event processing

---
+++ libecs/source/ControlSystem.cxx
@@ -0,0 +1,50 @@
+#include "pivot/ecs/Systems/ControlSystem.hxx"
+
+#include "pivot/ecs/Components/Gravity.hxx"
+#include "pivot/ecs/Components/RigidBody.hxx"
+#include "pivot/ecs/Components/Transform.hxx"
+#include "pivot/ecs/Core/Coordinator.hxx"
+
+extern Coordinator gCoordinator;
+
+void ControlSystem::Init()
+{
+	gCoordinator.AddEventListener(METHOD_LISTENER(Events::Window::INPUT, ControlSystem::InputListener));
+}
+
+void ControlSystem::Update(float dt)
+{
+    for (auto& entity : mEntities)
+	{
+		auto& transform = gCoordinator.GetComponent<Transform>(entity);
+
+
+		if (mButtons.test(static_cast<std::size_t>(InputButtons::W)))
+		{
+            std::cout << "W" << std::endl;
+			// transform.position.z += (dt * 10.0f);
+		}
+		else if (mButtons.test(static_cast<std::size_t>(InputButtons::S)))
+		{
+            std::cout << "S" << std::endl;
+			// transform.position.z -= (dt * 10.0f);
+		}
+
+
+		if (mButtons.test(static_cast<std::size_t>(InputButtons::A)))
+		{
+            std::cout << "A" << std::endl;
+			// transform.position.x += (dt * 10.0f);
+		}
+		else if (mButtons.test(static_cast<std::size_t>(InputButtons::D)))
+		{
+            std::cout << "D" << std::endl;
+			// transform.position.x -= (dt * 10.0f);
+		}
+	}
+}
+
+void ControlSystem::InputListener(Event& event)
+{
+	mButtons = event.GetParam<std::bitset<8>>(Events::Window::Input::INPUT);
+}

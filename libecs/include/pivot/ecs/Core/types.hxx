fdddc23eddae90590dbf3e600fe4ecd2e9bf4dc0
M@x <maxence.pellerin@epitech.eu>
2021-09-09 14:47:28 +0200

    feat(ECS): event processing

--- libecs/include/pivot/ecs/Core/types.hxx
+++ libecs/include/pivot/ecs/Core/types.hxx
@@ -2,6 +2,16 @@
 
 #include <bitset>
 
+constexpr std::uint32_t fnv1a_32(char const* s, std::size_t count)
+{
+	return ((count ? fnv1a_32(s, count - 1) : 2166136261u) ^ s[count]) * 16777619u; // NOLINT (hicpp-signed-bitwise)
+}
+
+constexpr std::uint32_t operator "" _hash(char const* s, std::size_t count)
+{
+	return fnv1a_32(s, count);
+}
+
 using Entity = std::uint32_t;
 const Entity MAX_ENTITIES = 5000;
 
@@ -12,3 +22,24 @@ using Signature = std::bitset<MAX_COMPONENTS>;
 
 using EventId = std::uint32_t;
 using ParamId = std::uint32_t;
+
+#define METHOD_LISTENER(EventType, Listener) EventType, std::bind(&Listener, this, std::placeholders::_1)
+#define FUNCTION_LISTENER(EventType, Listener) EventType, std::bind(&Listener, std::placeholders::_1)
+
+enum class InputButtons
+{
+	W,
+	A,
+	S,
+	D,
+	Q,
+	E
+};
+
+namespace Events::Window {
+const EventId INPUT = "Events::Window::INPUT"_hash;
+}
+
+namespace Events::Window::Input {
+const ParamId INPUT = "Events::Window::Input::INPUT"_hash;
+}

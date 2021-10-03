527c97e428226c419b7d500a43ac3e14146eb599
Zacharie Corniere <zacharie@corniere.me>
2021-10-03 10:17:43 +0200

    feat(ecs): moved camera to ECS

--- libecs/include/pivot/ecs/Systems/ControlSystem.hxx
+++ libecs/include/pivot/ecs/Systems/ControlSystem.hxx
@@ -1,18 +1,32 @@
 #pragma once
 
-#include "pivot/ecs/Core/System.hxx"
+#include "pivot/ecs/Components/Camera.hxx"
 #include "pivot/ecs/Core/Event.hxx"
+#include "pivot/ecs/Core/System.hxx"
 #include "pivot/graphics/Window.hxx"
+
+#include <optional>
 
 class ControlSystem : public System
 {
 public:
-	void Init();
+    static constexpr const double SPEED = 2.5;
+    static constexpr const double JUMP = 2.5;
+    static constexpr const double SENSITIVITY = 0.5;
+    static constexpr const bool CONSTRAIN_PITCH = true;
 
-	void Update(float dt);
+public:
+    void Init();
+
+    void Update(float dt);
 
 private:
-	std::bitset<UINT16_MAX> button;
+    std::bitset<UINT16_MAX> button;
+    std::optional<glm::dvec2> offset;
 
-	void InputListener(Event& event);
+    void InputListener(Event &event);
+    void MouseListener(Event &event);
+
+    static void processKeyboard(Camera &cam, const Camera::Movement direction) noexcept;
+    static void processMouseMovement(Camera &cam, const glm::dvec2 &offset);
 };

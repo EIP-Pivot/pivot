bcff7a98705847aa16663d9a759391767dc3f902
Zacharie Corniere <zacharie@corniere.me>
2021-10-03 10:17:43 +0200

    feat(ecs): moved camera to ECS

--- libecs/include/pivot/ecs/Components/Camera.hxx
+++ libecs/include/pivot/ecs/Components/Camera.hxx
@@ -1,7 +1,55 @@
 #pragma once
 
-#include <glm/glm.hpp>
+#include "pivot/graphics/interface/ICamera.hxx"
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
 
-struct Gravity {
-    glm::vec3 position;
+class Camera : public ICamera
+{
+public:
+    /// @cond
+    enum Movement { FORWARD, BACKWARD, LEFT, RIGHT, UP, DOWN };
+    static constexpr const double YAW = -90.0;
+    static constexpr const double PITCH = 0.0;
+    /// @endcond
+
+public:
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f))
+        : position(position), up(up)
+    {
+    }
+
+    glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 front = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 up;
+    glm::vec3 right;
+    glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
+    double yaw = YAW;
+    double pitch = PITCH;
+
+    GPUCameraData getGPUCameraData(float fFOV = 70.f, float fAspectRatio = 1700.f / 900.f,
+                                   float fCloseClippingPlane = 0.1,
+                                   float fFarClippingPlane = MAX_PROJECTION_LIMIT) const final
+    {
+        auto projection = glm::perspective(glm::radians(fFOV), fAspectRatio, fCloseClippingPlane, fFarClippingPlane);
+        projection[1][1] *= -1;
+        auto view = glm::lookAt(position, position + front, up);
+        GPUCameraData data{
+            .position = glm::vec4(position, 1.0f),
+            .viewproj = projection * view,
+        };
+        return data;
+    }
+    void updateCameraVectors()
+    {
+        glm::vec3 tmpFront;
+        tmpFront.x = std::cos(glm::radians(yaw)) * std::cos(glm::radians(pitch));
+        tmpFront.y = std::sin(glm::radians(pitch));
+        tmpFront.z = std::sin(glm::radians(yaw)) * std::cos(glm::radians(pitch));
+
+        front = glm::normalize(tmpFront);
+        right = glm::normalize(glm::cross(tmpFront, worldUp));
+        up = glm::normalize(glm::cross(right, tmpFront));
+    }
 };

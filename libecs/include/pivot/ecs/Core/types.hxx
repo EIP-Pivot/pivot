792e91664ca2be264263ed75dfc9ddc842a10143
Zacharie Corniere <zacharie@corniere.me>
2021-10-03 10:17:43 +0200

    feat(ecs): moved camera to ECS

--- libecs/include/pivot/ecs/Core/types.hxx
+++ libecs/include/pivot/ecs/Core/types.hxx
@@ -2,15 +2,12 @@
 
 #include <bitset>
 
-constexpr std::uint32_t fnv1a_32(char const* s, std::size_t count)
+constexpr std::uint32_t fnv1a_32(char const *s, std::size_t count)
 {
-	return ((count ? fnv1a_32(s, count - 1) : 2166136261u) ^ s[count]) * 16777619u; // NOLINT (hicpp-signed-bitwise)
+    return ((count ? fnv1a_32(s, count - 1) : 2166136261u) ^ s[count]) * 16777619u;    // NOLINT (hicpp-signed-bitwise)
 }
 
-constexpr std::uint32_t operator "" _hash(char const* s, std::size_t count)
-{
-	return fnv1a_32(s, count);
-}
+constexpr std::uint32_t operator"" _hash(char const *s, std::size_t count) { return fnv1a_32(s, count); }
 
 using Entity = std::uint32_t;
 const Entity MAX_ENTITIES = 5000;
@@ -23,10 +20,22 @@ using Signature = std::bitset<MAX_COMPONENTS>;
 using EventId = std::uint32_t;
 using ParamId = std::uint32_t;
 
-namespace Events::Window {
+namespace Events::Window
+{
 const EventId INPUT = "Events::Window::INPUT"_hash;
 }
 
-namespace Events::Window::Input {
+namespace Events::Window::Input
+{
 const ParamId INPUT = "Events::Window::Input::INPUT"_hash;
+}
+
+namespace Events::Window
+{
+const EventId MOUSE = "Events::Window::Mouse"_hash;
+}
+
+namespace Events::Window::Mouse
+{
+const ParamId MOUSE = "Events::Window::Mouse::MOUSE"_hash;
 }
--- libecs/source/Systems/ControlSystem.cxx
+++ libecs/source/Systems/ControlSystem.cxx
@@ -1,6 +1,5 @@
 #include "pivot/ecs/Systems/ControlSystem.hxx"
 
-#include "pivot/graphics/Camera.hxx"
 #include "pivot/ecs/Components/Transform.hxx"
 #include "pivot/ecs/Core/Coordinator.hxx"
 
@@ -8,61 +7,93 @@ extern Coordinator gCoordinator;
 
 void ControlSystem::Init()
 {
-	gCoordinator.AddEventListener(Events::Window::INPUT, std::bind(&ControlSystem::InputListener, this, std::placeholders::_1));
+    gCoordinator.AddEventListener(Events::Window::INPUT,
+                                  std::bind(&ControlSystem::InputListener, this, std::placeholders::_1));
+    gCoordinator.AddEventListener(Events::Window::MOUSE,
+                                  std::bind(&ControlSystem::MouseListener, this, std::placeholders::_1));
 }
 
 void ControlSystem::Update(float dt)
 {
-    try
-    {
-        for (auto& entity : mEntities)
-        {
-            auto& transform = gCoordinator.GetComponent<Transform>(entity);
-            auto& camera = gCoordinator.GetComponent<Camera>(entity);
+    try {
+        for (auto &entity: mEntities) {
+            auto &transform = gCoordinator.GetComponent<Transform>(entity);
+            auto &camera = gCoordinator.GetComponent<Camera>(entity);
 
-            if (button.test(static_cast<std::size_t>(Window::Key::W)))
-            {
-                camera.processKeyboard(Camera::FORWARD);
+            if (button.test(static_cast<std::size_t>(Window::Key::W))) {
+                processKeyboard(camera, Camera::FORWARD);
                 // transform.position.z += (dt * 10.0f);
-            }
-            else if (button.test(static_cast<std::size_t>(Window::Key::S)))
-            {
-                camera.processKeyboard(Camera::BACKWARD);
+            } else if (button.test(static_cast<std::size_t>(Window::Key::S))) {
+                processKeyboard(camera, Camera::BACKWARD);
                 // transform.position.z -= (dt * 10.0f);
             }
 
-
-            if (button.test(static_cast<std::size_t>(Window::Key::A)))
-            {
-                camera.processKeyboard(Camera::LEFT);
+            if (button.test(static_cast<std::size_t>(Window::Key::A))) {
+                processKeyboard(camera, Camera::LEFT);
                 // transform.position.x += (dt * 10.0f);
-            }
-            else if (button.test(static_cast<std::size_t>(Window::Key::D)))
-            {
-                camera.processKeyboard(Camera::RIGHT);
+            } else if (button.test(static_cast<std::size_t>(Window::Key::D))) {
+                processKeyboard(camera, Camera::RIGHT);
                 // transform.position.x -= (dt * 10.0f);
             }
 
-            if (button.test(static_cast<std::size_t>(Window::Key::SPACE)))
-            {
-                camera.processKeyboard(Camera::UP);
+            if (button.test(static_cast<std::size_t>(Window::Key::SPACE))) {
+                processKeyboard(camera, Camera::UP);
                 // transform.position.x += (dt * 10.0f);
-            }
-            else if (button.test(static_cast<std::size_t>(Window::Key::LEFT_SHIFT)))
-            {
-                camera.processKeyboard(Camera::DOWN);
+            } else if (button.test(static_cast<std::size_t>(Window::Key::LEFT_SHIFT))) {
+                processKeyboard(camera, Camera::DOWN);
                 // transform.position.x -= (dt * 10.0f);
+            }
+            if (offset) {
+                ControlSystem::processMouseMovement(camera, *offset);
+                offset = std::nullopt;
             }
         }
-    }
-    catch(const std::exception& e)
-    {
+    } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
-    
 }
 
-void ControlSystem::InputListener(Event& event)
+void ControlSystem::InputListener(Event &event)
 {
     button = event.GetParam<std::bitset<UINT16_MAX>>(Events::Window::Input::INPUT);
+}
+
+void ControlSystem::MouseListener(Event &event) { offset = event.GetParam<glm::dvec2>(Events::Window::Mouse::MOUSE); }
+
+void ControlSystem::processKeyboard(Camera &cam, const Camera::Movement direction) noexcept
+{
+    switch (direction) {
+        case Camera::Movement::FORWARD: {
+            cam.position.x += cam.front.x * SPEED;
+            cam.position.z += cam.front.z * SPEED;
+        } break;
+        case Camera::Movement::BACKWARD: {
+            cam.position.x -= cam.front.x * SPEED;
+            cam.position.z -= cam.front.z * SPEED;
+        } break;
+        case Camera::Movement::RIGHT: {
+            cam.position.x += cam.right.x * SPEED;
+            cam.position.z += cam.right.z * SPEED;
+        } break;
+        case Camera::Movement::LEFT: {
+            cam.position.x -= cam.right.x * SPEED;
+            cam.position.z -= cam.right.z * SPEED;
+        } break;
+        case Camera::Movement::UP: {
+            cam.position.y += JUMP;
+        } break;
+        case Camera::Movement::DOWN: cam.position.y -= SPEED; break;
+    }
+}
+
+void ControlSystem::processMouseMovement(Camera &cam, const glm::dvec2 &offset)
+{
+    cam.yaw += offset.x * SENSITIVITY;
+    cam.pitch += offset.y * SENSITIVITY;
+
+    if (CONSTRAIN_PITCH) {
+        if (cam.pitch > 89.0f) cam.pitch = 89.0f;
+        if (cam.pitch < -89.0f) cam.pitch = -89.0f;
+    }
+    cam.updateCameraVectors();
 }

02e68c4b7fce324651cf57eff7f9ca8bac14ebe1
Zacharie Corniere <zacharie@corniere.me>
2021-10-12 15:19:52 +0200

    feat(libgraphics): remove CPU-side transform

--- libgraphics/include/pivot/graphics/types/UniformBufferObject.hxx
+++ libgraphics/include/pivot/graphics/types/UniformBufferObject.hxx
@@ -5,21 +5,27 @@
 #include <glm/mat4x4.hpp>
 #include <string>
 
+namespace gpuObject
+{
+/// @struct Transform
+///
+/// @brief Hold the model matrices
+struct Transform {
+    /// The translation matrix
+    glm::mat4 translation;
+    /// The rotation matrix
+    glm::mat4 rotation;
+    /// The scale matrix
+    glm::mat4 scale;
+};
+}    // namespace gpuObject
+
 /// @class ObjectInformation
 ///
 /// @brief Hold the information of the 3D object, on the CPU-side
 struct ObjectInformation {
-    /// @brief Contain the position, rotation and scale of the 3D object
-    struct Transform {
-        /// The object position in 3D space
-        glm::vec3 translation;
-        /// The object rotation, in radians
-        glm::vec3 rotation;
-        /// The scale of the object
-        glm::vec3 scale;
-    };
     /// The object transform
-    Transform transform;
+    gpuObject::Transform transform;
     /// The name of the texture to apply on the object
     std::string textureIndex;
     /// The name of the material to use on the object
@@ -28,29 +34,12 @@ struct ObjectInformation {
 
 namespace gpuObject
 {
-/// @struct Transform
-///
-/// @brief Hold the model matrices
-struct Transform {
-    /// Constructor from the CPU-side transform
-    ///
-    /// @param cpuTransform The CPU side of the model transformation
-    Transform(const ObjectInformation::Transform &cpuTransform);
-    /// The translation matrix
-    glm::mat4 translation;
-    /// The rotation matrix
-    glm::mat4 rotation;
-    /// The scale matrix
-    glm::mat4 scale;
-};
 
 /// @struct UniformBufferObject
 ///
 /// @brief Hold all the information of the 3D object
 struct UniformBufferObject {
-    /// Constructor from the CPU-side object information
-    ///
-    /// @param info The CPU object information
+    /// @param info The object information
     /// @param imageStor The texture storage, used to resolve the name of the texture
     /// @param materialStor The material storage, used to resolve material name
     UniformBufferObject(const ObjectInformation &info, const ImageStorage &imageStor,

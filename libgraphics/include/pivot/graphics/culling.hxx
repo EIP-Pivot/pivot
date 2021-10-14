0eec02d9bb28c33c64e9c6aa76d14bd222df08b2
Ersikan <julien.philippon@epitech.eu>
2021-10-14 22:03:45 +0200

    refactor(graphics): Take only `gpuObject::Transform` as culling input

--- libgraphics/include/pivot/graphics/culling.hxx
+++ libgraphics/include/pivot/graphics/culling.hxx
@@ -1,12 +1,12 @@
 #ifndef PIVOT_ENGINE_GRAPHICS_CULLING_HXX
 #define PIVOT_ENGINE_GRAPHICS_CULLING_HXX
 
-#include <pivot/graphics/types/RenderObject.hxx>
+#include <pivot/graphics/types/UniformBufferObject.hxx>
 #include <pivot/graphics/types/vk_types.hxx>
 
 namespace pivot::graphics::culling
 {
-bool should_object_be_rendered(const RenderObject &object, const MeshBoundingBox &box,
+bool should_object_be_rendered(const Transform &transform, const MeshBoundingBox &box,
                                const gpuObject::CameraData &camera);
 }
 

23b90717146f8987ff63f3d233e8fce6ac30719c
Ersikan <julien.philippon@epitech.eu>
2021-10-14 23:15:57 +0200

    refactor(graphics): Extract bounding box calculations

--- libgraphics/include/pivot/graphics/types/Mesh.hxx
+++ libgraphics/include/pivot/graphics/types/Mesh.hxx
@@ -33,10 +33,25 @@ struct GPUMesh {
 ///
 /// @brief Represents the cubic bounding box of a mesh
 struct MeshBoundingBox {
+    MeshBoundingBox() = delete;
+    explicit MeshBoundingBox(glm::vec3 initialPoint): low(initialPoint), high(initialPoint){};
+    MeshBoundingBox(glm::vec3 low, glm::vec3 high): low(low), high(high){};
+
     /// Lowest point of the bouding box
     glm::vec3 low;
     /// Highest point of the bounding box
     glm::vec3 high;
+
+    /// Add a point to the bounding box
+    void addPoint(glm::vec3 point)
+    {
+        high.x = std::max(point.x, high.x);
+        high.y = std::max(point.y, high.y);
+        high.z = std::max(point.z, high.z);
+        low.x = std::min(point.x, low.x);
+        low.y = std::min(point.y, low.y);
+        low.z = std::min(point.z, low.z);
+    }
 
     /// Returns an array of the 8 vertices of the bounding box
     std::array<glm::vec3, 8> vertices() const

ff5a3be500f757644886aeb53db4b13dddca5528
Maxence Pellerin <maxence.pellerin@epitech.eu>
2021-10-16 11:57:01 +0200

    feat(Ecs): documentation

--- libecs/include/pivot/ecs/Core/Event.hxx
+++ libecs/include/pivot/ecs/Core/Event.hxx
@@ -4,32 +4,37 @@
 #include <any>
 #include <unordered_map>
 
-/*! \cond
- */
+/// @class Event
+///
+/// @brief Send event to Event Manager
 class Event
 {
 public:
+    /// @cond
     Event() = delete;
+    /// @endcond
 
+    /// Create event
     explicit Event(EventId type): mType(type) {}
 
+    /// Set event parameter
     template <typename T>
     void SetParam(ParamId id, T value)
     {
         mData[id] = value;
     }
 
+    /// Get event parameter
     template <typename T>
     T GetParam(ParamId id)
     {
         return std::any_cast<T>(mData[id]);
     }
 
+    /// Get event type
     EventId GetType() const { return mType; }
 
 private:
     EventId mType{};
     std::unordered_map<EventId, std::any> mData{};
 };
-/*! \endcond
- */
--- libecs/include/pivot/ecs/Systems/ControlSystem.hxx
+++ libecs/include/pivot/ecs/Systems/ControlSystem.hxx
@@ -7,19 +7,30 @@
 
 #include <optional>
 
+/// @class ControlSystem
+///
+/// @brief Control system example, to control camera movement
 class ControlSystem : public System
 {
 public:
+    /// Speed movement
     static constexpr const float SPEED = 2.5f;
+    /// Jump height
     static constexpr const float JUMP = 2.5f;
+    /// Sensitivity of the mouse
     static constexpr const float SENSITIVITY = 0.5f;
+    /// @cond
     static constexpr const bool CONSTRAIN_PITCH = true;
+    /// @endcond
 
 public:
+    /// Init system
     void Init();
 
+    /// Update camera movement when key press
     virtual void Update(float dt);
 
+    /// Update mouse movement (Called in Update()) 
     static void processMouseMovement(Camera &cam, const glm::dvec2 &offset);
 private:
     std::bitset<UINT16_MAX> button;

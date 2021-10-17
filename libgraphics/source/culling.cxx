40c5d07a640c1fec307b0494cbbabac12a7359bb
Ersikan <julien.philippon@epitech.eu>
2021-10-17 15:11:42 +0200

    feat(graphics): Compute projection matrix only once

--- libgraphics/source/culling.cxx
+++ libgraphics/source/culling.cxx
@@ -8,9 +8,10 @@ namespace pivot::graphics::culling
 bool should_object_be_rendered(const Transform &transform, const MeshBoundingBox &box,
                                const gpuObject::CameraData &camera)
 {
+    auto projection = camera.viewproj * transform.getModelMatrix();
     bool result = false;
     for (const auto &point: box.vertices()) {
-        const auto position = camera.viewproj * transform.getModelMatrix() * glm::vec4(point.x, point.y, point.z, 1.0);
+        const auto position = projection * glm::vec4(point.x, point.y, point.z, 1.0);
         result = result || (position.x <= position.w && position.x >= -position.w && position.y <= position.w &&
                             position.y >= -position.w && position.z <= position.w && position.z >= 0);
     }

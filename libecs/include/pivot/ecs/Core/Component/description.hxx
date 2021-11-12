685f543c29ee47230b5668533eeefa3221eb9eb1
Ersikan <julien.philippon@epitech.eu>
2021-11-12 22:20:02 +0100

    feat(ecs): Component description structure

---
+++ libecs/include/pivot/ecs/Core/Component/description.hxx
@@ -0,0 +1,46 @@
+#include <any>
+#include <functional>
+#include <map>
+#include <string>
+#include <variant>
+#include <vector>
+#include <stdexcept>
+
+#ifndef PIVOT_ECS_COMPONENT_DESCRIPTION_HPP
+#define PIVOT_ECS_COMPONENT_DESCRIPTION_HPP
+
+namespace pivot::ecs::component
+{
+
+struct Description {
+    struct Property {
+        std::string name;
+        enum class Type {
+            STRING,
+            NUMBER,
+            ASSET,
+        } type;
+
+        using ValueType = std::variant<int, std::string>;
+    };
+
+    std::string name;
+    std::vector<Property> properties;
+
+    using GetPropertyType = Property::ValueType (&)(std::any component, std::string property);
+    using SetPropertyType = void (&)(std::any component, std::string property, Property::ValueType value);
+    using CreateType = std::any (&)(std::map<std::string, Property::ValueType> properties);
+
+    GetPropertyType getProperty;
+    SetPropertyType setProperty;
+    CreateType create;
+
+    void validate() const;
+
+        class ValidationError : public std::logic_error  {
+                using std::logic_error::logic_error;
+        };
+};
+}    // namespace pivot::ecs::component
+
+#endif    // PIVOT_ECS_COMPONENT_DESCRIPTION_HPP
---
+++ libecs/source/Core/Component/description.cxx
@@ -0,0 +1,28 @@
+#include <pivot/ecs/Core/Component/description.hxx>
+#include <set>
+
+namespace pivot::ecs::component
+{
+
+void Description::validate() const
+{
+    if (this->name.empty()) { throw ValidationError("Empty component name"); }
+
+    std::set<std::string> property_names;
+
+    for (auto &[name, type]: this->properties) {
+        if (name.empty()) { throw ValidationError("Empty property name"); }
+
+        if (property_names.contains(name)) { throw ValidationError("Duplicate property name"); }
+
+        const std::set<Description::Property::Type> all_property_types{
+            Description::Property::Type::STRING,
+            Description::Property::Type::NUMBER,
+            Description::Property::Type::ASSET,
+        };
+        if (!all_property_types.contains(type)) { throw ValidationError("Unknown property type"); }
+
+        property_names.insert(name);
+    }
+}
+}    // namespace pivot::ecs::component

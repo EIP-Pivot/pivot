768dcf1160e8a9736c8bb81f193160708227dc5a
Ersikan <julien.philippon@epitech.eu>
2021-11-22 11:54:48 +0100

    feat(ecs): Allow component containers to return ref to components

--- libecs/include/pivot/ecs/Core/Component/array.hxx
+++ libecs/include/pivot/ecs/Core/Component/array.hxx
@@ -14,7 +14,8 @@ public:
     virtual ~IComponentArray() = default;
 
     virtual const Description &getDescription() const = 0;
-    virtual std::optional<std::any> getValueForEntity(Entity entity) = 0;
+    virtual std::optional<std::any> getValueForEntity(Entity entity) const = 0;
+    virtual std::optional<std::any> getRefForEntity(Entity entity) = 0;
     virtual void setValueForEntity(Entity entity, std::optional<std::any>) = 0;
 };
 }    // namespace pivot::ecs::component

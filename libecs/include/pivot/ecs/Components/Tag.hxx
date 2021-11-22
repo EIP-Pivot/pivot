ddd74bbcb7359f9a2510164b16fe281a960261b1
Ersikan <julien.philippon@epitech.eu>
2021-11-22 11:58:21 +0100

    feat(ecs): Add description of Tag component

--- libecs/include/pivot/ecs/Components/Tag.hxx
+++ libecs/include/pivot/ecs/Components/Tag.hxx
@@ -2,10 +2,14 @@
 
 #include <string>
 
+#include <pivot/ecs/Core/Component/description.hxx>
+
 /// @struct Tag
 ///
 /// @brief Tag component, use to name entities
 struct Tag {
     /// Name of an entity
     std::string name;
+
+    static pivot::ecs::component::Description description;
 };
---
+++ libecs/source/Components/Tag.cxx
@@ -0,0 +1,55 @@
+#include <pivot/ecs/Components/Tag.hxx>
+#include <pivot/ecs/Core/Component/array.hxx>
+#include <pivot/ecs/Core/Component/error.hxx>
+
+using namespace pivot::ecs::component;
+
+namespace
+{
+
+Description::Property::ValueType getProperty(std::any component, std::string property)
+{
+    if (property != "name") { throw UnknownProperty(property); }
+    auto *tag = std::any_cast<Tag>(&component);
+    if (tag == nullptr) { throw InvalidComponent("Tag", typeid(tag), component.type()); }
+    return Description::Property::ValueType(tag->name);
+}
+
+void setProperty(std::any component, std::string property, Description::Property::ValueType value)
+{
+    if (property != "name") { throw UnknownProperty(property); }
+    auto *tag = std::any_cast<Tag>(&component);
+    if (tag == nullptr) { throw InvalidComponent("Tag", typeid(tag), component.type()); }
+    auto new_name = std::get_if<std::string>(&value);
+    if (new_name == nullptr) {
+        throw PropertyTypeError(property, Description::Property::Type::STRING, Description::Property::Type::NUMBER);
+    }
+    tag->name = *new_name;
+}
+
+std::any create(std::map<std::string, Description::Property::ValueType> properties)
+{
+    auto name_it = properties.find("name");
+
+    if (name_it == properties.end()) { throw MissingProperty("name"); }
+
+    if (properties.size() != 1) {
+        for (const auto &property: properties) {
+            if (property.first != "name") { throw UnknownProperty(property.first); }
+        }
+    }
+
+    auto name = std::get_if<std::string>(&name_it->second);
+    if (name == nullptr) {
+        throw PropertyTypeError("name", Description::Property::Type::STRING, Description::Property::Type::NUMBER);
+    }
+
+    return std::make_any<Tag>(*name);
+}
+
+std::unique_ptr<IComponentArray> createContainer() { return std::unique_ptr<IComponentArray>(nullptr); }
+}    // namespace
+
+Description Tag::description{"Tag",       {{"name", pivot::ecs::component::Description::Property::Type::STRING}},
+                             getProperty, setProperty,
+                             create,      createContainer};

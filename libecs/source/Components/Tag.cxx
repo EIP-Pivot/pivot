5b195f0d1c15b004ee4760fd224f82223b5d3489
Ersikan <julien.philippon@epitech.eu>
2021-11-30 13:48:43 +0100

    feat(ecs): Generate component access function automatically

--- libecs/source/Components/Tag.cxx
+++ libecs/source/Components/Tag.cxx
@@ -14,51 +14,15 @@ using namespace pivot::ecs::component;
 
 BOOST_FUSION_ADAPT_STRUCT(Tag, name);
 
-namespace
+namespace pivot::ecs::component::helpers
 {
-
-Description::Property::ValueType getProperty(std::any component, std::string property)
-{
-    if (property != "name") { throw UnknownProperty(property); }
-    auto *tag = std::any_cast<Tag>(&component);
-    if (tag == nullptr) { throw InvalidComponent("Tag", typeid(tag), component.type()); }
-    return Description::Property::ValueType(tag->name);
+template <>
+constexpr const char *component_name<Tag> = "Tag";
 }
 
-void setProperty(std::any &component, std::string property, Description::Property::ValueType value)
-{
-    if (property != "name") { throw UnknownProperty(property); }
-    auto *tag = std::any_cast<Tag>(&component);
-    if (tag == nullptr) { throw InvalidComponent("Tag", typeid(tag), component.type()); }
-    auto new_name = std::get_if<std::string>(&value);
-    if (new_name == nullptr) {
-        throw PropertyTypeError(property, Description::Property::Type::STRING, Description::Property::Type::NUMBER);
-    }
-    tag->name = *new_name;
-}
-
-std::any create(std::map<std::string, Description::Property::ValueType> properties)
-{
-    auto name_it = properties.find("name");
-
-    if (name_it == properties.end()) { throw MissingProperty("name"); }
-
-    if (properties.size() != 1) {
-        for (const auto &property: properties) {
-            if (property.first != "name") { throw UnknownProperty(property.first); }
-        }
-    }
-
-    auto name = std::get_if<std::string>(&name_it->second);
-    if (name == nullptr) {
-        throw PropertyTypeError("name", Description::Property::Type::STRING, Description::Property::Type::NUMBER);
-    }
-
-    return std::make_any<Tag>(*name);
-}
-
-std::unique_ptr<IComponentArray> createContainer() { return std::unique_ptr<IComponentArray>(nullptr); }
-}    // namespace
-
-Description Tag::description{"Tag",          helpers::buildPropertyArray<Tag>(), getProperty, setProperty, create,
-                             createContainer};
+Description Tag::description{"Tag",
+                             helpers::buildPropertyArray<Tag>(),
+                             helpers::getProperty<Tag>,
+                             helpers::setProperty<Tag>,
+                             helpers::create<Tag>,
+                             helpers::createContainer<Tag>};

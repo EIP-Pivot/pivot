027b36190a8bd411599206399d720c7083536fa2
Maxteke <maxence.pellerin@logipro.com>
2022-01-10 13:38:22 +0100

    fix(traits)

--- libecs/include/pivot/ecs/Core/Systems/function_traits.hxx
+++ libecs/include/pivot/ecs/Core/Systems/function_traits.hxx
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <pivot/ecs/Core/Component/index.hxx>
+
+#include <array>
 #include <cstddef>
 #include <tuple>
 
@@ -20,12 +23,21 @@ template <typename ReturnType, typename... Args>
 struct function_traits<ReturnType (*)(Args...)> {
     static constexpr size_t arity = sizeof...(Args);
 
+
+
+
     using result_type = ReturnType;
 
     using args_type = std::tuple<Args...>;
-
+    
     template <size_t i>
     struct arg {
         using type = typename std::tuple_element<i, std::tuple<Args...>>::type;
     };
+
+    static std::array<std::optional<std::string>, arity> getArgsName() {
+         return {
+            ((pivot::ecs::component::GlobalIndex::getSingleton().getComponentNameByType<Args>()), ...)
+         };
+    }
 };

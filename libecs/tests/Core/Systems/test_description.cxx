25c1f94351cd9a7d2adf715766fad219a9d3e970
Maxteke <maxence.pellerin@logipro.com>
2022-01-10 13:38:22 +0100

    fix(traits)

---
+++ libecs/tests/Core/Systems/test_description.cxx
@@ -0,0 +1,25 @@
+#include <catch2/catch.hpp>
+#include <pivot/ecs/Core/Systems/description.hxx>
+#include "pivot/ecs/Components/Tag.hxx"
+#include "pivot/ecs/Components/Gravity.hxx"
+#include "pivot/ecs/Components/RigidBody.hxx"
+
+
+#include <iostream>
+
+using namespace pivot::ecs::systems;
+
+void coucou(Tag, Gravity, RigidBody)
+{
+}
+
+TEST_CASE("system description", "[systems]")
+{
+    const std::string test = "Control";
+    Description description = Description::build_system_description(test, &coucou);
+    std::cout << "Function name: " << description.name << std::endl;
+    for (auto arg: description.arguments)
+    {
+        std::cout << "Function args: " << *arg << std::endl;
+    }
+}

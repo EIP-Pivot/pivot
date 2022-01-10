ede6f8a79c7933cd1e46c4e96991701197ea1b0e
Maxence Pellerin <maxence.pellerin@epitech.eu>
2022-01-10 14:20:58 +0100

    feat(description): done

--- libecs/tests/Core/Systems/test_description.cxx
+++ libecs/tests/Core/Systems/test_description.cxx
@@ -1,9 +1,8 @@
-#include <catch2/catch.hpp>
-#include <pivot/ecs/Core/Systems/description.hxx>
-#include "pivot/ecs/Components/Tag.hxx"
 #include "pivot/ecs/Components/Gravity.hxx"
 #include "pivot/ecs/Components/RigidBody.hxx"
-
+#include "pivot/ecs/Components/Tag.hxx"
+#include <catch2/catch.hpp>
+#include <pivot/ecs/Core/Systems/description.hxx>
 
 #include <iostream>
 
@@ -11,13 +10,32 @@ using namespace pivot::ecs::systems;
 
 void coucou(RigidBody, Tag, Gravity) {}
 
-TEST_CASE("system description", "[systems]")
+TEST_CASE("valid system description", "[systems]")
 {
     const std::string test = "Control";
     Description description = Description::build_system_description(test, &coucou);
-    std::cout << "Function name: " << description.name << std::endl;
-    for (auto arg: description.arguments)
-    {
-        std::cout << "Function args: " << *arg << std::endl;
-    }
+    REQUIRE_NOTHROW(description.validate());
+}
+
+TEST_CASE("Empty system description", "[systems]")
+{
+    Description description;
+    REQUIRE_THROWS_WITH(description.validate(), "Empty system name");
+}
+
+TEST_CASE("Empty args system description", "[systems]")
+{
+    Description description{
+        .name = "Invalid",
+    };
+    REQUIRE_THROWS_WITH(description.validate(), "Empty system argument");
+}
+
+TEST_CASE("Invalid args system description", "[systems]")
+{
+    Description description{
+        .name = "Invalid",
+        .arguments = {"NOT REGISTERED"},
+    };
+    REQUIRE_THROWS_WITH(description.validate(), Catch::Matchers::Contains("Component ") && Catch::Matchers::Contains(" his not registered."));
 }

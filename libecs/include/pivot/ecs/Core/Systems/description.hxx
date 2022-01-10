5002ca9762feb1706faf74ae966e4b9436239c42
Maxence Pellerin <maxence.pellerin@epitech.eu>
2022-01-10 14:20:58 +0100

    feat(description): done

--- libecs/include/pivot/ecs/Core/Systems/description.hxx
+++ libecs/include/pivot/ecs/Core/Systems/description.hxx
@@ -3,15 +3,16 @@
 #include "pivot/ecs/Core/Systems/function_traits.hxx"
 
 #include <functional>
+#include <iostream>
 #include <stdexcept>
 #include <string>
-#include <iostream>
+
 
 namespace pivot::ecs::systems
 {
 
-template<typename Fn>
-concept SystemFunction = requires 
+template <typename Fn>
+concept SystemFunction = requires
 {
     std::is_same_v<typename function_traits<Fn>::result_type, void>;
     function_traits<Fn>::arity > 0;
@@ -21,21 +22,22 @@ struct Description {
 
     std::string name;
 
-    std::vector<std::optional<std::string>> arguments;
-
-    std::function<void(float, Description)> system;
+    std::vector<std::string> arguments;
 
     void validate() const;
 
     static Description build_system_description(const std::string &name, SystemFunction auto function)
     {
-        Description newSystem;
-        newSystem.name = name;
+        Description newSystem {
+            .name = name,
+        };
 
         typedef function_traits<decltype(function)> traits;
 
-        const auto &n = traits::getArgsName();
-        newSystem.arguments = std::vector(n.begin(), n.end());
+        for (const auto &arg: traits::getArgsName()) {
+            if (!arg.has_value()) throw ValidationError("Component not registered.");
+            newSystem.arguments.push_back(arg.value());
+        }
         return newSystem;
     }
 
@@ -45,4 +47,4 @@ struct Description {
     };
 };
 
-}
+}    // namespace pivot::ecs::systems

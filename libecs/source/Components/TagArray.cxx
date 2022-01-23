4c7c7eab88dfd8f1c0b951da9dceac9a91ae5886
Ersikan <julien.philippon@epitech.eu>
2022-01-23 17:58:38 +0100

    fix(ecs): TagArray defers to DenseComponentArry for insertion

--- libecs/source/Components/TagArray.cxx
+++ libecs/source/Components/TagArray.cxx
@@ -4,17 +4,17 @@ namespace pivot::ecs::component
 {
 void TagArray::setValueForEntity(Entity entity, std::optional<std::any> value)
 {
-    if (!value.has_value()) {
-        if (m_component_exist.at(entity)) { m_tag_names.erase(m_components.at(entity).name); }
-        m_component_exist.at(entity) = false;
-    } else {
-        auto tag = std::any_cast<Tag>(value.value());
-
-        if (m_tag_names.contains(tag.name)) { throw TagArray::DuplicateEntityTag(tag.name); }
-        m_tag_names.insert(tag.name);
-
-        m_components.at(entity) = tag;
-        m_component_exist.at(entity) = true;
+    // Remove old tag from tag set
+    if (entity < m_component_exist.size() && m_component_exist[entity]) {
+        m_tag_names.erase(m_components[entity].name);
     }
+
+    // Add new tag to tag set
+    if (value.has_value()) {
+        auto tag = std::any_cast<Tag>(*value);
+        m_tag_names.insert(tag.name);
+    }
+
+    this->DenseTypedComponentArray<Tag>::setValueForEntity(entity, value);
 }
 }    // namespace pivot::ecs::component

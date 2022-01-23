a9f7350e065cb63f85b7181a6c2b95a7fc7324cf
Zacharie Corniere <zacharie@corniere.me>
2022-01-23 20:06:08 +0100

    feat(UniformBufferObject): remove index resolution macro

--- libgraphics/source/types/UniformBufferObject.cxx
+++ libgraphics/source/types/UniformBufferObject.cxx
@@ -1,14 +1,17 @@
 #include "pivot/graphics/types/UniformBufferObject.hxx"
 
-#define CAT(A, B) A##B
-#define CHECK_DEFAULT(ASSET, TYPE)                                                                                   \
-    if (obj.objectInformation.CAT(ASSET, Index)) {                                                                   \
-        CAT(ASSET, Index) = assetStorage.getIndex<TYPE>(obj.objectInformation.CAT(ASSET, Index).value());            \
-    } else if (model.default_##ASSET) {                                                                              \
-        CAT(ASSET, Index) = assetStorage.getIndex<TYPE>(model.default_##ASSET.value());                              \
-    } else {                                                                                                         \
-        throw pivot::graphics::AssetStorage::AssetStorageException("No " #ASSET " defined for " + obj.meshID + "!"); \
-    }
+template <class T>
+static inline std::optional<std::uint32_t> getDefault(const pivot::graphics::AssetStorage &stor,
+                                                      const std::optional<std::string> &value,
+                                                      const std::optional<std::string> &defaultValue)
+{
+    if (value)
+        return stor.getIndex<T>(value.value());
+    else if (defaultValue)
+        return stor.getIndex<T>(defaultValue.value());
+    else
+        return std::nullopt;
+}
 
 gpuObject::UniformBufferObject::UniformBufferObject(const RenderObject &obj,
                                                     const pivot::graphics::AssetStorage &assetStorage)
@@ -17,7 +20,22 @@ gpuObject::UniformBufferObject::UniformBufferObject(const RenderObject &obj,
 {
     const auto &model = assetStorage.get<pivot::graphics::AssetStorage::Model>(obj.meshID);
 
-    CHECK_DEFAULT(texture, pivot::graphics::AssetStorage::Texture);
-    CHECK_DEFAULT(material, gpuObject::Material);
+    {
+        auto tmpIndex = getDefault<pivot::graphics::AssetStorage::Texture>(
+            assetStorage, obj.objectInformation.textureIndex, model.default_texture);
+        if (tmpIndex)
+            throw pivot::graphics::AssetStorage::AssetStorageException("Missing texture for "
+                                                                       "obj " +
+                                                                       obj.meshID);
+        textureIndex = tmpIndex.value();
+    }
+    {
+        auto tmpIndex =
+            getDefault<gpuObject::Material>(assetStorage, obj.objectInformation.materialIndex, model.default_material);
+        if (tmpIndex)
+            throw pivot::graphics::AssetStorage::AssetStorageException("Missing material for "
+                                                                       "obj " +
+                                                                       obj.meshID);
+    }
     boundingBoxIndex = assetStorage.getIndex<MeshBoundingBox>(obj.meshID);
 }

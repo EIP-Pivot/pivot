cdfd9ed8b58a1f5a6a2011093ae2fb310fc3919f
Ersikan <julien.philippon@epitech.eu>
2022-01-25 15:08:29 +0100

    refactor(ecs): Use Type and Value for component descriptions and arrays

--- libecs/include/pivot/ecs/Core/Component/array.hxx
+++ libecs/include/pivot/ecs/Core/Component/array.hxx
@@ -5,6 +5,7 @@
 
 #include <pivot/ecs/Core/Component/description.hxx>
 #include <pivot/ecs/Core/types.hxx>
+#include <pivot/ecs/Core/Data/value.hxx>
 
 namespace pivot::ecs::component
 {
@@ -26,13 +27,9 @@ public:
     virtual const Description &getDescription() const = 0;
 
     /// Returns the value of the component associated with a given entity, if any.
-    virtual std::optional<std::any> getValueForEntity(Entity entity) const = 0;
-
-    // HACK: Remove this as soon as it is not needed anymore
-    /// Does the same as getValueForEntity(), but returns a std::reference_wrapper of the type
-    virtual std::optional<std::any> getRefForEntity(Entity entity) = 0;
+    virtual std::optional<data::Value> getValueForEntity(Entity entity) const = 0;
 
     /// Sets the value of the component for an entity
-    virtual void setValueForEntity(Entity entity, std::optional<std::any>) = 0;
+    virtual void setValueForEntity(Entity entity, std::optional<data::Value>) = 0;
 };
 }    // namespace pivot::ecs::component
--- libecs/include/pivot/ecs/Core/Component/description.hxx
+++ libecs/include/pivot/ecs/Core/Component/description.hxx
@@ -11,6 +11,8 @@
 
 #include <glm/vec3.hpp>
 
+#include <pivot/ecs/Core/Data/type.hxx>
+
 /** \brief Contains everything related to components creation and usage
  *
  * The components are the *data* part of the ECS. They store every property a
@@ -35,75 +37,11 @@ class IComponentArray;
  */
 struct Description {
 
-    /** \brief A property (member) of a component
-     *
-     * A component is composed of multiple named and typed properties. For now
-     * components can only contain a flat list of named properties and their
-     * corresponding types, meaning they cannot be nested.
-     *
-     * All the property names of a component must be unique, and no property is
-     * allowed to have an empty name.
-     */
-    struct Property {
-        /// Type of a property. It must always be a valid value of the Type enum.
-        enum class Type {
-            /// The property is of type std::string
-            String,
-
-            /// The property is of type int
-            Number,
-
-            /// Unused for now
-            Asset,
-
-            /// The property is of type glm::vec3
-            Vec3,
-        };
-
-        // TODO: Store the type with the value
-
-        /// The type of component's property value
-        using ValueType = std::variant<int, std::string, glm::vec3>;
-
-        /// Name of a property
-        std::string name;
-
-        /// The type of the property
-        Type type;
-    };
-
     /// The name of the component. It cannot be empty
     std::string name;
 
-    /// The list of properties of the component.
-    std::vector<Property> properties;
-
-    /** \brief Type of the property getter of a component
-     *
-     * This function is used to access a named property of a component.
-     *
-     * \param component The value of the component. It must be a value created with CreateContainerType.
-     * \param property The name of the property to access
-     */
-    using GetPropertyType = Property::ValueType(std::any component, std::string property);
-
-    /** \brief Type of the property setter of a component
-     *
-     * This function is used to set the value of a named property of a component.
-     *
-     * \param component The value of the component. It must be a value created with CreateContainerType.
-     * \param property The name of the property to access.
-     * \param value The value to put in the property. It must be of the same type as described in Property::Type
-     */
-    using SetPropertyType = void(std::any &component, std::string property, Property::ValueType value);
-
-    /** \brief Type of the factory of the component
-     *
-     * This function is used to create a component, based on values of its properties.
-     *
-     * \param properties A std::map containing all the properties of the new component and their value.
-     */
-    using CreateType = std::any(std::map<std::string, Property::ValueType> properties);
+    /// The type of values storeds in this component
+    data::Type type;
 
     /** \brief Type of the factory of the IComponentArray of the component
      *
@@ -114,12 +52,6 @@ struct Description {
      */
     using CreateContainerType = std::unique_ptr<IComponentArray>(Description description);
 
-    /// Functor allowing to retrieve the value of a property of the component
-    GetPropertyType &getProperty;
-    /// Functor allowing to set the value of a property of the component
-    SetPropertyType &setProperty;
-    /// Functor creating a new instance of the component using the provided property and value list
-    CreateType &create;
     /// Functor creating an instance of the IComponentArray type associated with this component
     CreateContainerType &createContainer;
 

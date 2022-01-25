f76317adf914e9b6ac8dee2757c3a5f4f20f6c76
Maxence Pellerin <maxence.pellerin@epitech.eu>
2022-01-25 17:49:30 +0100

    feat(event): index + description + manager

---
+++ libecs/source/Core/Event/manager.cxx
@@ -0,0 +1,15 @@
+#include "pivot/ecs/Core/Event/manager.hxx"
+
+namespace pivot::ecs::event
+{
+
+void Manager::registerEvent(const Description &description)
+{
+    
+}
+
+void Manager::sendEvent(const std::string &eventName, const data::Value &value, const Entities &entities)
+{
+
+}
+}

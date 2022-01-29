b3a1a7237ba6514824655d88d3778b97ea5582de
Ersikan <julien.philippon@epitech.eu>
2022-01-29 18:34:06 +0100

    test(ecs): Test component refs

---
+++ libecs/tests/Core/Component/test_ref.cxx
@@ -0,0 +1,28 @@
+#include <catch2/catch.hpp>
+
+#include <pivot/ecs/Core/Component/index.hxx>
+#include <pivot/ecs/Core/Component/ref.hxx>
+
+using namespace pivot::ecs::data;
+using namespace pivot::ecs::component;
+
+TEST_CASE("Component refs works", "[component]")
+{
+    auto description = GlobalIndex::getSingleton().getDescription("Tag").value();
+    auto array = description.createContainer(description);
+
+    ComponentRef ref(*array, 0);
+    REQUIRE_THROWS(ref.get());
+
+    Value original_tag{Record{{"name", "bob"}}};
+    array->setValueForEntity(0, original_tag);
+    REQUIRE(ref.get() == original_tag);
+    REQUIRE(static_cast<Value>(ref) == original_tag);
+
+    Value new_tag{Record{{"name", "alice"}}};
+    ref.set(new_tag);
+    REQUIRE(array->getValueForEntity(0) == new_tag);
+
+    ref = original_tag;
+    REQUIRE(array->getValueForEntity(0) == original_tag);
+}

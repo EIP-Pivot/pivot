8b6d5f4cf212da534bf92d17d14743af11252901
Maxence Pellerin <maxence.pellerin@epitech.eu>
2022-01-29 18:30:01 +0100

    feat(event): add vector of entities components

--- libecs/tests/Core/Systems/test_manager.cxx
+++ libecs/tests/Core/Systems/test_manager.cxx
@@ -4,24 +4,25 @@
 #include "pivot/ecs/Core/EntityManager.hxx"
 #include "pivot/ecs/Core/Systems/manager.hxx"
 #include <catch2/catch.hpp>
+#include <pivot/ecs/Core/Data/value.hxx>
 #include <pivot/ecs/Core/Scene.hxx>
 #include <pivot/ecs/Core/Systems/description.hxx>
-#include <pivot/ecs/Core/Data/value.hxx>
 
 using namespace pivot::ecs;
 using namespace pivot::ecs::data;
 
-void test_manager_registration(const systems::Description::availableEntities &e, const systems::Description &description,
+void test_manager_registration(const systems::Description &description,
                                const systems::Description::systemArgs &entities, const event::Event &event)
 {
     std::cout << "I'm a tick system:\n";
     auto &tagArray = entities[1].get();
-    for (const auto &entity: e) {
-        auto tag = tagArray.getValueForEntity(entity).value();
-        std::cout << std::get<std::string>(std::get<Record>(tag).at("name")) << std::endl;
-        std::get<std::string>(std::get<Record>(tag).at("name")) = "edit";
-        tagArray.setValueForEntity(entity, tag);
-    }
+    auto tag = tagArray.getValueForEntity(0).value();
+    std::cout << std::get<std::string>(std::get<Record>(tag).at("name")) << std::endl;
+    std::get<std::string>(std::get<Record>(tag).at("name")) = "edit";
+    tagArray.setValueForEntity(0, tag);
+    for (const auto &enitity: event.entities)
+        for (const auto &value: enitity)
+            std::cout << value.type() << std::endl;
 }
 
 TEST_CASE("Manager register system", "[description][registration][manager]")
@@ -30,12 +31,9 @@ TEST_CASE("Manager register system", "[description][registration][manager]")
     std::unique_ptr<EntityManager> eManager = std::make_unique<EntityManager>();
     systems::Manager manager(cManager, eManager);
 
-    component::Description tag =
-        component::GlobalIndex::getSingleton().getDescription("Tag").value();
-    component::Description rigid =
-        component::GlobalIndex::getSingleton().getDescription("RigidBody").value();
-    component::Description grav =
-        component::GlobalIndex::getSingleton().getDescription("Gravity").value();
+    component::Description tag = component::GlobalIndex::getSingleton().getDescription("Tag").value();
+    component::Description rigid = component::GlobalIndex::getSingleton().getDescription("RigidBody").value();
+    component::Description grav = component::GlobalIndex::getSingleton().getDescription("Gravity").value();
 
     component::Manager::ComponentId tagId = cManager->RegisterComponent(tag);
     component::Manager::ComponentId rigidId = cManager->RegisterComponent(rigid);
@@ -45,7 +43,7 @@ TEST_CASE("Manager register system", "[description][registration][manager]")
     Entity entity = eManager->CreateEntity();
     cManager->AddComponent(entity, Value{Record{{"name", "entity 0"}}}, tagId);
     cManager->AddComponent(entity, Value{Record{{"velocity", glm::vec3(0.0f)}, {"acceleration", glm::vec3(0.0f)}}},
-                          rigidId);
+                           rigidId);
     cManager->AddComponent(entity, Value{Record{{"force", glm::vec3(0.0f)}}}, gravId);
 
     // entity that match with system
@@ -61,29 +59,29 @@ TEST_CASE("Manager register system", "[description][registration][manager]")
                            rigidId);
     cManager->AddComponent(entity, Value{Record{{"force", glm::vec3(0.0f)}}}, gravId);
 
-    event::Description eventDescription {
+    event::Description eventDescription{
         .name = "Tick",
-        .entities = {},
+        .entities = {
+            {"Oui"}
+        },
         .payload = pivot::ecs::data::BasicType::Number,
     };
     systems::Description description{
         .name = "Manager",
-        .components =
+        .systemComponents =
             {
                 "RigidBody",
                 "Tag",
             },
         .eventListener = eventDescription,
+        .eventComponents = {
+            {"RigidBody"}
+        },
         .system = &test_manager_registration,
     };
     systems::GlobalIndex::getSingleton().registerSystem(description);
     manager.useSystem(description);
 
-    event::Event event {
-        .description = eventDescription,
-        .entities = {},
-        .payload = data::Value{1},
-    };
-    manager.execute(event);
-    manager.execute(event);
+    manager.execute(eventDescription, data::Value{1}, {0});
+    manager.execute(eventDescription, data::Value{1}, {1});
 }

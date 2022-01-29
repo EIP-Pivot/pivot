9153fdc05a1847f7e0bb2b70a0fb1ddce0b0867d
Maxence Pellerin <maxence.pellerin@epitech.eu>
2022-01-29 21:07:02 +0100

    feat(scripting): scripting component array

---
+++ libecs/source/Core/Component/ScriptingComponentArray.cxx
@@ -0,0 +1,25 @@
+#include "pivot/ecs/Core/Component/ScriptingComponentArray.hxx"
+
+namespace pivot::ecs::component
+{
+
+const Description &ScriptingComponentArray::getDescription() const
+{ 
+    return m_description;
+}
+
+std::optional<data::Value> ScriptingComponentArray::getValueForEntity(Entity entity) const
+{
+    auto value = m_components.find(entity);
+    if (value == m_components.end())
+        return std::nullopt;
+    return value->second;
+}
+
+void ScriptingComponentArray::setValueForEntity(Entity entity, std::optional<data::Value> value)
+{
+    m_components[entity] = value.value();
+}
+
+
+}

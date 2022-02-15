39abb047a3959030ac5a51ee4723b4be73bfaaaa
Ersikan <julien.philippon@epitech.eu>
2022-02-15 19:15:28 +0100

    test(pivot): Test transform wrapper

--- libpivot/tests/components/test_render_object_component.cxx
+++ libpivot/tests/components/test_render_object_component.cxx
@@ -1,6 +1,29 @@
 #include <catch2/catch.hpp>
+#include <nlohmann/json.hpp>
 
+#include <pivot/ecs/Core/Component/description_helpers.hxx>
 #include <pivot/ecs/Core/Component/index.hxx>
+#include <pivot/ecs/Core/Data/value_serialization.hxx>
 #include <pivot/graphics/types/RenderObject.hxx>
 
-TEST_CASE("transform adapter work", "[graphics]") {}
+using namespace nlohmann;
+using namespace pivot::ecs::data;
+using namespace pivot::ecs::component;
+
+TEST_CASE("transform adapter work", "[graphics]")
+{
+    Transform transform;
+
+    Value transform_value = json::parse(R"({"scale": [1,2,3], "position": [5, 5, 5], "rotation": [0, 2.7, 0.4]})");
+    helpers::Helpers<Transform>::updateTypeWithValue(transform, transform_value);
+
+    Transform expected;
+    expected.setPosition({5, 5, 5});
+    expected.setRotation({0, 2.7, 0.4});
+    expected.setScale({1, 2, 3});
+    Value expected_value = helpers::Helpers<Transform>::createValueFromType(expected);
+
+    REQUIRE(transform_value.type() == expected_value.type());
+    REQUIRE(transform_value == expected_value);
+    REQUIRE(transform == expected);
+}

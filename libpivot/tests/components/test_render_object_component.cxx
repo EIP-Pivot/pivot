c8455675c79a0c2a79172be597d4e0020eef0694
Ersikan <julien.philippon@epitech.eu>
2022-02-22 14:57:55 +0100

    test(pivot): Test render object component

--- libpivot/tests/components/test_render_object_component.cxx
+++ libpivot/tests/components/test_render_object_component.cxx
@@ -1,6 +1,8 @@
 #include <catch2/catch.hpp>
 #include <nlohmann/json.hpp>
 
+#include <pivot/ecs/Core/Component/DenseComponentArray.hxx>
+#include <pivot/ecs/Core/Component/array.hxx>
 #include <pivot/ecs/Core/Component/description_helpers.hxx>
 #include <pivot/ecs/Core/Component/index.hxx>
 #include <pivot/ecs/Core/Data/value_serialization.hxx>
@@ -26,4 +28,22 @@ TEST_CASE("transform adapter work", "[graphics]")
     REQUIRE(transform_value.type() == expected_value.type());
     REQUIRE(transform_value == expected_value);
     REQUIRE(transform == expected);
+}
+
+TEST_CASE("RenderObject component works", "[graphics][component]")
+{
+    auto description = GlobalIndex::getSingleton().getDescription("RenderObject").value();
+    auto array = description.createContainer(description);
+
+    const auto data =
+        R"({"meshID": "sponza","materialIndex":"blue","transform":{"scale": [1,2,3], "position": [5, 5, 5], "rotation": [0, 2.7, 0.4]}})";
+
+    REQUIRE_NOTHROW(array->setValueForEntity(0, json::parse(data).get<Value>()));
+
+    const RenderObject expected{
+        .meshID = "sponza", .materialIndex = "blue", .transform = Transform({5, 5, 5}, {0, 2.7, 0.4}, {1, 2, 3})
+
+    };
+    const RenderObject &renderObject = dynamic_cast<DenseTypedComponentArray<RenderObject> &>(*array).getData().front();
+    REQUIRE(renderObject == expected);
 }

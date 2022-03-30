3a5c5d77072f5d05cda02a6d5857c0b04987179f
Zacharie Corniere <zacharie@corniere.me>
2022-03-30 12:21:45 +0200

    feat(Material): fixed material alignment

--- libgraphics/include/pivot/graphics/types/Material.hxx
+++ libgraphics/include/pivot/graphics/types/Material.hxx
@@ -9,12 +9,12 @@ namespace pivot::graphics::gpu_object
 /// @struct Material
 /// @brief Represent a GPU side material
 struct Material {
+    /// Base color of the material, if no baseColorTexture is provided
+    alignas(16) glm::vec4 baseColor = glm::vec4(1.0f);
     /// Metalic factor
     alignas(4) float metallic = 1.0f;
     /// roughness factor
     alignas(4) float roughness = 1.0f;
-    /// Base color of the material, if no baseColorTexture is provided
-    alignas(16) glm::vec4 baseColor = glm::vec4(1.0f);
     /// Diffuse texture, use to determine the base color of the fragment
     alignas(4) std::int32_t baseColorTexture = -1;
     /// Metallic/Roughness Texture
@@ -28,6 +28,6 @@ struct Material {
 };
 
 static_assert(sizeof(Material) % 4 == 0);
-// static_assert(sizeof(Material) == ((sizeof(float) * 2) + (sizeof(float) * 4) + (sizeof(std::int32_t) * 5)));
+static_assert(sizeof(Material) == ((sizeof(float) * 4) + (sizeof(float) * 2) + (sizeof(std::int32_t) * 5) + 4));
 
 }    // namespace pivot::graphics::gpu_object

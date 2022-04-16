04a1c722fbf44bace48a0ae7be415a4b6f82d113
Ersikan <julien.philippon@epitech.eu>
2022-04-17 00:04:23 +0200

    refactor(pivot,editor): Use engine to save scenes

--- libpivot/include/pivot/engine.hxx
+++ libpivot/include/pivot/engine.hxx
@@ -22,6 +22,7 @@ public:
     void changeCurrentScene(ecs::SceneManager::SceneId sceneId);
     ecs::SceneManager::SceneId registerScene();
     ecs::SceneManager::SceneId registerScene(std::string name);
+    void saveScene(ecs::SceneManager::SceneId id, const std::filesystem::path &path);
 
 protected:
     pivot::ecs::component::Index m_component_index;

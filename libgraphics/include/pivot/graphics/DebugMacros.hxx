ab66c1f6a41962b9d3eb76e740fa53ca30540afb
Zacharie Corniere <zacharie@corniere.me>
2022-04-20 09:14:06 +0200

    feat(DebugMacros): use std::source_location instead of __PRETTY_FUNCTION__

--- libgraphics/include/pivot/graphics/DebugMacros.hxx
+++ libgraphics/include/pivot/graphics/DebugMacros.hxx
@@ -1,13 +1,30 @@
 #pragma once
 
 #include <Logger.hpp>
+#include <source_location>
 
-#if !defined(__PRETTY_FUNCTION__) && !defined(__GNUC__)
-    #define __PRETTY_FUNCTION__ __FUNCSIG__
-#endif
+namespace pivot
+{
+/// Return the name of the calling function
+constexpr std::string_view
+function_name(const std::source_location &location = std::source_location::current()) noexcept
+{
+    return location.function_name();
+}
+
+/// @brief Return the position of the calling function
+///
+/// Format: "filename":"line":"column"
+inline std::string_view file_position(const std::source_location &location = std::source_location::current()) noexcept
+{
+    return std::string() + location.file_name() + ":" + std::to_string(location.line()) + ":" +
+           std::to_string(location.column());
+}
+
+}    // namespace pivot
 
 #ifdef NDEBUG
     #define DEBUG_FUNCTION
 #else
-    #define DEBUG_FUNCTION logger LOGGER_ACCESS trace(__PRETTY_FUNCTION__) << "Entered";
+    #define DEBUG_FUNCTION logger LOGGER_ACCESS trace(::pivot::function_name()) << "Entered";
 #endif

f9c4260eebb617914b301324cf2c6aeffd2a0b60
Zacharie Corniere <zacharie@corniere.me>
2022-04-29 10:21:20 +0200

    fix(VulkanException): fixed inheritance losing the exception message

--- libgraphics/include/pivot/graphics/VulkanException.hxx
+++ libgraphics/include/pivot/graphics/VulkanException.hxx
@@ -8,19 +8,42 @@ namespace pivot::graphics::internal
 class PivotException : public std::exception
 {
 public:
+    PivotException() = delete;
+    /// main ctor
+    PivotException(const std::string &msg): msg(msg) {}
     /// return the scope of the exception
     virtual const char *const getScope() const noexcept = 0;
+    /// return the kind of the exception
+    virtual const char *const getKind() const noexcept = 0;
+    /// return both of the above
+
+    /// return the error message
+    const char *what() const noexcept override { return msg.c_str(); }
+
+private:
+    const std::string msg;
 };
+
+class PivotLogicError : public PivotException
+{
+    using PivotException::PivotException;
+    const char *const getKind() const noexcept final { return "Logic"; };
+};
+
+class PivotRuntimeError : public PivotException
+{
+    using PivotException::PivotException;
+    const char *const getKind() const noexcept final { return "Logic"; };
+};
+
 }    // namespace pivot::graphics::internal
 
-#define RUNTIME_EXCEPTION(name)                                                                          \
-    struct name##Error : public std::runtime_error, public ::pivot::graphics::internal::PivotException { \
-        using std::runtime_error::runtime_error;                                                         \
-        const char *const getScope() const noexcept override { return #name; }                           \
+#define PIVOT_EXCEPTION(kind, name)                                            \
+    struct name##Error : public ::pivot::graphics::internal::kind {            \
+        using kind::kind;                                                      \
+        const char *const getScope() const noexcept override { return #name; } \
     };
 
-#define LOGIC_EXCEPTION(name)                                                                          \
-    struct name##Error : public std::logic_error, public ::pivot::graphics::internal::PivotException { \
-        using std::logic_error::logic_error;                                                           \
-        const char *const getScope() const noexcept override { return #name; }                         \
-    }
+#define RUNTIME_EXCEPTION(name) PIVOT_EXCEPTION(PivotRuntimeError, name)
+
+#define LOGIC_EXCEPTION(name) PIVOT_EXCEPTION(PivotLogicError, name)

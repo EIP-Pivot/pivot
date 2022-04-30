d819b9754bbdc378521109c6ffb27d3069727b09
Ersikan <julien.philippon@epitech.eu>
2022-04-30 12:54:25 +0200

    feat(pivot): Complete physics system

--- libpivot/sources/builtins/systems/PhysicSystem.cxx
+++ libpivot/sources/builtins/systems/PhysicSystem.cxx
@@ -1,25 +1,36 @@
 #include <pivot/builtins/events/tick.hxx>
 #include <pivot/builtins/systems/PhysicSystem.hxx>
+#include <pivot/ecs/Core/Component/DenseComponentArray.hxx>
+
+#include <pivot/builtins/components/RenderObject.hxx>
+#include <pivot/ecs/Components/Gravity.hxx>
+#include <pivot/ecs/Components/RigidBody.hxx>
 
 using namespace pivot::ecs;
+using namespace pivot::builtins::components;
+
 namespace
 {
-void physicsSystemImpl(const systems::Description &systemDescription, component::ArrayCombination &entities,
+void physicsSystemImpl(const systems::Description &systemDescription, component::ArrayCombination &cmb,
                        const event::EventWithComponent &event)
 {
     auto dt = (float)std::get<double>(event.event.payload);
-    for (auto combination: entities) {
-        auto gravity = combination[0].get();
-        auto rigidBody = combination[1].get();
 
-        auto &force = std::get<glm::vec3>(std::get<data::Record>(gravity).at("force"));
-        force.x += 1;
+    auto gravityArray = dynamic_cast<component::DenseTypedComponentArray<Gravity> &>(cmb.arrays()[0].get()).getData();
+    auto rigidBodyArray =
+        dynamic_cast<component::DenseTypedComponentArray<RigidBody> &>(cmb.arrays()[1].get()).getData();
+    auto renderObjectArray =
+        dynamic_cast<component::DenseTypedComponentArray<RenderObject> &>(cmb.arrays()[2].get()).getData();
 
-        combination[0].set(gravity);
+    auto maxEntity = std::min({gravityArray.size(), rigidBodyArray.size(), renderObjectArray.size()});
+    for (std::size_t entity = 0; entity <= maxEntity; entity++) {
+        auto &gravity = gravityArray[entity];
+        auto &rigidBody = rigidBodyArray[entity];
+        auto &renderObject = renderObjectArray[entity];
 
-        auto &velocity = std::get<glm::vec3>(std::get<data::Record>(rigidBody).at("velocity"));
-        velocity += force * dt;
-        combination[1].set(rigidBody);
+        if (gravity.force != glm::vec3(0)) { rigidBody.acceleration = gravity.force; }
+        rigidBody.velocity += rigidBody.acceleration * dt;
+        renderObject.transform.position += rigidBody.velocity * dt;
     }
 }
 }    // namespace
@@ -32,9 +43,9 @@ const pivot::ecs::systems::Description physicSystem{
         {
             "Gravity",
             "RigidBody",
+            "RenderObject",
         },
     .eventListener = events::tick,
     .system = &physicsSystemImpl,
 };
-
 }

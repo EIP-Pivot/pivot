17ff745e57edc8ac16abfc14f2e1e21349f10303
Zacharie Corniere <zacharie@corniere.me>
2022-04-30 12:20:08 +0200

    feat(VulkanException): use std::runtime_error as a base
    also make sure the types are nothrow copy constructible && assignable

--- libgraphics/include/pivot/graphics/VulkanException.hxx
+++ libgraphics/include/pivot/graphics/VulkanException.hxx
@@ -5,25 +5,14 @@
 namespace pivot
 {
 /// Base class for pivot exceptions
-class PivotException : public std::exception
+class PivotException : public std::runtime_error
 {
 public:
-    PivotException() = delete;
-    /// dtor
-    virtual ~PivotException(){};
-    /// main ctor
-    PivotException(const std::string &msg): msg(msg) {}
+    using std::runtime_error::runtime_error;
     /// return the scope of the exception
     virtual const std::string_view getScope() const noexcept = 0;
     /// return the kind of the exception
     virtual const std::string_view getKind() const noexcept = 0;
-    /// return both of the above
-
-    /// return the error message
-    const char *what() const noexcept override { return msg.c_str(); }
-
-private:
-    const std::string msg;
 };
 
 /// Pivot Logic Error
@@ -46,7 +35,9 @@ class RuntimeError : public PivotException
     struct name##Error : public ::pivot::kind {                                     \
         using kind::kind;                                                           \
         const std::string_view getScope() const noexcept override { return #name; } \
-    };
+    };                                                                              \
+    static_assert(std::is_nothrow_copy_constructible_v<name##Error>);               \
+    static_assert(std::is_nothrow_copy_assignable_v<name##Error>);
 
 #define RUNTIME_EXCEPTION(name) PIVOT_EXCEPTION(RuntimeError, name)
 

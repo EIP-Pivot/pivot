80b445212c9024ee45ae59905d91ff401c2ad843
Najo-z <jonathan.hugonnard@gmail.com>
2022-05-01 22:51:49 +0200

    fix(script): throw runtime_errors too also clang format

--- libscript/include/pivot/script/Exceptions.hxx
+++ libscript/include/pivot/script/Exceptions.hxx
@@ -8,26 +8,26 @@ namespace pivot::ecs::script
 
 class MixedIndentException : public std::runtime_error
 {
-    size_t linenb;             // line of the indent error
-    const char *line;          // raw line of the error
-    const char *info;          // info on the error
 public:
     MixedIndentException(const char *msg, const char *line_, size_t linenb_, const char *info_)
         : std::runtime_error(msg), line(line_), linenb(linenb_), info(info_)
     {
     }
+    /// return the error line number
     size_t get_line_nb() const { return linenb; }
+    /// return the error raw line
     const char *get_line() const { return line; }
+    /// return the error specification
     const char *get_info() const { return info; }
+
+private:
+    size_t linenb;
+    const char *line;
+    const char *info;
 };
 
 class TokenException : public std::runtime_error
 {
-    const char *token;            // token that produced the error
-    size_t linenb;                // line position of the error
-    size_t charnb;                // char position on the line
-    const char *exctype;          // WARNING (non-blocking) or ERROR (blocking)
-    const char *info;             // specification
 public:
     TokenException(const char *exceptionType, const char *token_, size_t linenb_, size_t charnb_, const char *exctype_,
                    const char *info_)
@@ -39,26 +39,43 @@ public:
           info(info_)
     {
     }
+    /// return the token that caused the error
     const char *get_token() const { return token; }
+    /// return the error line number
     size_t get_line_nb() const { return linenb; }
+    /// return the error char number
     size_t get_char_nb() const { return charnb; }
+    /// return the error type
     const char *get_exctype() const { return exctype; }
+    /// return the error specification
     const char *get_info() const { return info; }
+
+private:
+    const char *token;
+    size_t linenb;
+    size_t charnb;
+    const char *exctype;
+    const char *info;
 };
 
 class InvalidException : public std::runtime_error
 {
-    const char *exceptionType;    // precision to the type of error
-    const char *faulter;          // repr of what caused the error
-    const char *error;            // specification
 public:
     InvalidException(const char *exceptionType_, const char *faulter_, const char *error_)
         : std::runtime_error(exceptionType_), exceptionType(exceptionType_), faulter(faulter_), error(error_)
     {
     }
+    /// return the error type
     const char *getExceptionType() const { return exceptionType; }
+    /// return the representation of what caused the error
     const char *getFaulter() const { return faulter; }
+    /// return the error specification
     const char *getError() const { return error; }
+
+private:
+    const char *exceptionType;
+    const char *faulter;
+    const char *error;
 };
 
 }    // end of namespace pivot::ecs::script

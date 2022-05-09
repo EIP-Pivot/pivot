e9fd98f4747e24b06c228b19fd65a15c8d983626
Ersikan <julien.philippon@epitech.eu>
2022-05-09 20:02:19 +0200

    doc(ecs): Document Asset

--- libecs/include/pivot/ecs/Core/Data/asset.hxx
+++ libecs/include/pivot/ecs/Core/Data/asset.hxx
@@ -4,9 +4,12 @@
 
 namespace pivot::ecs::data
 {
+/// Value containing a reference to an asset loaded in the engine
 struct Asset {
+    /// Name of the asset
     std::string name;
 
+    /// Compare two assets by their name
     auto operator<=>(const Asset &rhs) const = default;
 };
 }    // namespace pivot::ecs::data

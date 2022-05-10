0a14aae38e847963ad0fce7858f603e20fd6dd22
Ersikan <julien.philippon@epitech.eu>
2022-05-10 13:32:34 +0200

    feat(pivot): Complete new RenderObject and Transform components

---
+++ libpivot/sources/builtins/components/Transform.cxx
@@ -0,0 +1,12 @@
+#include "pivot/builtins/components/Transform.hxx"
+#include "pivot/ecs/Core/Component/DenseComponentArray.hxx"
+#include "pivot/ecs/Core/Component/description_helpers_impl.hxx"
+#include "pivot/graphics/types/Transform.hxx"
+
+#include <boost/fusion/include/adapt_struct.hpp>
+
+using namespace pivot::builtins::components;
+
+BOOST_FUSION_ADAPT_STRUCT(pivot::graphics::Transform, position, rotation, scale);
+PIVOT_REGISTER_WRAPPED_COMPONENT(Transform, pivot::graphics::Transform,
+                                 DenseTypedComponentArray<pivot::graphics::Transform>);

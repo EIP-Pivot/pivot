45913ae9a25dd3f40ecdec926e208bf48a41e772
Ersikan <julien.philippon@epitech.eu>
2022-05-22 12:03:58 +0200

    test(script): Test the equality operator builtin

--- libscript/tests/test_builtins.cxx
+++ libscript/tests/test_builtins.cxx
@@ -4,6 +4,8 @@
 #include <pivot/script/Builtins.hxx>
 
 using namespace pivot::ecs;
+using namespace pivot::ecs::data;
+using namespace pivot::ecs::script::interpreter::builtins;
 
 std::string print(const std::vector<data::Value> &params)
 {
@@ -20,4 +22,17 @@ TEST_CASE("Builtin print", "[script][builtin]")
     REQUIRE(print({true}) == "true\n");
     REQUIRE(print({data::Asset{"cube"}}) == "Asset(cube)\n");
     REQUIRE(print({glm::vec3(1, 2, 3)}) == "vec3(1,2,3)\n");
+}
+
+bool equal(const data::Value &left, const data::Value &right)
+{
+    return std::get<bool>(builtin_operator<Operator::Equal>(left, right));
+}
+
+TEST_CASE("Builtin equal", "[script][builtin]")
+{
+    REQUIRE(equal(Value{3}, Value{3}));
+    REQUIRE(equal(Value{"test"}, Value{"test"}));
+    REQUIRE_FALSE(equal(Value{3}, Value{4}));
+    REQUIRE_THROWS(equal(Value{"haha"}, Value{2}));
 }

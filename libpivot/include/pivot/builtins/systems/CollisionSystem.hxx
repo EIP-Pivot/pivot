b871fc12f026476ce59d19ef29d60f78dcd364e2
Ersikan <julien.philippon@epitech.eu>
2022-06-06 16:43:19 +0200

    test(pivot): Test collision system

--- libpivot/include/pivot/builtins/systems/CollisionSystem.hxx
+++ libpivot/include/pivot/builtins/systems/CollisionSystem.hxx
@@ -6,4 +6,16 @@
 namespace pivot::builtins::systems
 {
 const pivot::ecs::systems::Description makeCollisionSystem(const pivot::graphics::AssetStorage &assetStorage);
-}
+
+namespace details
+{
+
+    struct EntityAABB {
+        glm::vec3 low;
+        glm::vec3 high;
+        Entity entity;
+    };
+
+    std::vector<std::pair<Entity, Entity>> getEntityCollisions(std::span<const EntityAABB> entityAABB);
+}    // namespace details
+}    // namespace pivot::builtins::systems
--- libpivot/sources/builtins/systems/CollisionSystem.cxx
+++ libpivot/sources/builtins/systems/CollisionSystem.cxx
@@ -10,16 +10,11 @@
 
 using namespace pivot::ecs;
 using namespace pivot::builtins::components;
+using namespace pivot::builtins::systems::details;
 using AABB = pivot::graphics::gpu_object::AABB;
 
 namespace
 {
-struct GlobalAABB {
-    glm::vec3 low;
-    glm::vec3 high;
-    Entity entity;
-};
-
 void collisionSystemImpl(const pivot::graphics::AssetStorage &assetStorage,
                          const systems::Description &systemDescription, component::ArrayCombination &cmb,
                          const event::EventWithComponent &event)
@@ -32,7 +27,7 @@ void collisionSystemImpl(const pivot::graphics::AssetStorage &assetStorage,
         dynamic_cast<component::DenseTypedComponentArray<pivot::graphics::RenderObject> &>(cmb.arrays()[2].get())
             .getData();
 
-    std::vector<GlobalAABB> globalAABB;
+    std::vector<EntityAABB> entityAABB;
 
     for (Entity entity: collidableStorage.getData()) {
         // TODO: Do the collision checks I guess ?
@@ -40,26 +35,11 @@ void collisionSystemImpl(const pivot::graphics::AssetStorage &assetStorage,
         auto &mesh = renderObjectArray[entity].meshID;
         auto bounding_box = assetStorage.get_optional<AABB>(mesh);
         if (bounding_box.has_value()) {
-            globalAABB.emplace_back(bounding_box->get().low + position, bounding_box->get().high + position, entity);
+            entityAABB.emplace_back(bounding_box->get().low + position, bounding_box->get().high + position, entity);
         }
     }
 
-    std::vector<std::pair<Entity, Entity>> collisions;
-
-    for (auto box1: globalAABB) {
-        for (auto box2: globalAABB) {
-            if (box1.entity == box2.entity) continue;
-            float d1x = box2.low.x - box1.high.x;
-            float d1y = box2.low.y - box1.high.y;
-            float d2x = box1.low.x - box2.high.x;
-            float d2y = box1.low.y - box2.high.y;
-
-            if (d1x > 0.0f || d1y > 0.0f) continue;
-            if (d2x > 0.0f || d2y > 0.0f) continue;
-
-            collisions.emplace_back(box1.entity, box2.entity);
-        }
-    }
+    auto collisions = getEntityCollisions(entityAABB);
 
     for (auto [entity1, entity2]: collisions) {
         logger.debug() << "Collision between entity " << entity1 << " and entity " << entity2;
@@ -69,6 +49,33 @@ void collisionSystemImpl(const pivot::graphics::AssetStorage &assetStorage,
 
 namespace pivot::builtins::systems
 {
+namespace details
+{
+
+    std::vector<std::pair<Entity, Entity>> getEntityCollisions(std::span<const EntityAABB> entityAABB)
+    {
+        std::vector<std::pair<Entity, Entity>> collisions;
+
+        for (std::size_t i = 0; i < entityAABB.size(); i++) {
+            auto &box1 = entityAABB[i];
+            for (auto &box2: entityAABB.subspan(i)) {
+                if (box1.entity == box2.entity) continue;
+                float d1x = box2.low.x - box1.high.x;
+                float d1y = box2.low.y - box1.high.y;
+                float d2x = box1.low.x - box2.high.x;
+                float d2y = box1.low.y - box2.high.y;
+
+                if (d1x > 0.0f || d1y > 0.0f) continue;
+                if (d2x > 0.0f || d2y > 0.0f) continue;
+
+                collisions.emplace_back(box1.entity, box2.entity);
+            }
+        }
+        return collisions;
+    }
+
+}    // namespace details
+
 const pivot::ecs::systems::Description makeCollisionSystem(const pivot::graphics::AssetStorage &assetStorage)
 {
     return {

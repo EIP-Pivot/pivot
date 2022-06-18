db4dc9aeff15116a974eca651d31b662e0d2f0fe
Zacharie Corniere <zacharie@corniere.me>
2022-06-18 15:10:19 +0200

    chore(pivot): move the source location to pivot.hxx

--- libgraphics/include/pivot/graphics/DebugMacros.hxx
+++ libgraphics/include/pivot/graphics/DebugMacros.hxx
@@ -1,27 +1,8 @@
 #pragma once
 
+#include "pivot/graphics/pivot.hxx"
+
 #include <Logger.hpp>
-#include <source_location>
-
-namespace pivot
-{
-/// Return the name of the calling function
-constexpr std::string_view
-function_name(const std::source_location &location = std::source_location::current()) noexcept
-{
-    return location.function_name();
-}
-
-/// @brief Return the position of the calling function
-///
-/// Format: "filename":"line":"column"
-inline std::string_view file_position(const std::source_location &location = std::source_location::current()) noexcept
-{
-    return std::string() + location.file_name() + ":" + std::to_string(location.line()) + ":" +
-           std::to_string(location.column());
-}
-
-}    // namespace pivot
 
 #ifdef NDEBUG
     #define DEBUG_FUNCTION

051838d5dc7345f61a726e9945ff0e6f94970ce8
Ersikan <julien.philippon@epitech.eu>
2022-08-13 09:59:02 +0200

    fix(ecs): Check entity tag duplication

--- libecs/tests/Core/Component/test_tag.cxx
+++ libecs/tests/Core/Component/test_tag.cxx
@@ -1,5 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <pivot/ecs/Components/Tag.hxx>
+#include <pivot/ecs/Components/TagArray.hxx>
 #include <pivot/ecs/Core/Component/description_helpers.hxx>
 #include <pivot/ecs/Core/Component/index.hxx>
 
@@ -27,4 +28,18 @@ TEST_CASE("Correct description for tag component", "[component][tag]")
 
     REQUIRE_THROWS(Helpers<Tag>::updateTypeWithValue(tag, Value{3}));
     REQUIRE_THROWS(Helpers<Tag>::updateTypeWithValue(tag, Value{Record{}}));
+}
+
+TEST_CASE("Tag array rejects duplicated tags", "[component][ecs]")
+{
+    TagArray array(Tag::description);
+
+    auto name1 = Value{Record{{"name", "name"}}};
+    auto name2 = Value{Record{{"name", "other name"}}};
+
+    REQUIRE_NOTHROW(array.setValueForEntity(0, name1));
+    REQUIRE_NOTHROW(array.setValueForEntity(1, name2));
+    REQUIRE_THROWS_AS(array.setValueForEntity(2, name1), TagArray::DuplicateEntityTag);
+    REQUIRE_NOTHROW(array.setValueForEntity(0, std::nullopt));
+    REQUIRE_NOTHROW(array.setValueForEntity(2, name1));
 }

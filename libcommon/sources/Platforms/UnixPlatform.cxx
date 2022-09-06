9bc8b4bb7613e57e26757b063e1ef38a11aca1da
Zacharie Corniere <zacharie@corniere.me>
2022-09-06 21:41:26 +0200

    chore(UnixPlatform): use strstr to find the debugger

--- libcommon/sources/Platforms/UnixPlatform.cxx
+++ libcommon/sources/Platforms/UnixPlatform.cxx
@@ -7,6 +7,7 @@
     #include <string.h>
     #include <sys/utsname.h>
     #include <unistd.h>
+    #include <cstring>
 
 namespace pivot
 {
@@ -23,21 +24,25 @@ bool UnixPlateform::isDebuggerPresent()
     }
 
     char Buffer[256];
-    ssize_t Length = read(StatusFile, Buffer, sizeof(Buffer));
+    Buffer[255] = 0;
+    ssize_t Length = read(StatusFile, Buffer, sizeof(Buffer) - 1);
+    if (Length == -1)  {
+        // Failed - unknown debugger status.
+        return false;
+    }
+
     close(StatusFile);
 
     constexpr char TracerString[] = "TracerPid:\t";
-    constexpr ssize_t LenTracerString = std::size(TracerString);
+    const ssize_t LenTracerString = std::strlen(TracerString);
 
-    for (int i = 0; (Length - i) > LenTracerString; i++) {
-        // TracerPid is found
-        if (strncmp(&Buffer[i], TracerString, LenTracerString) == 0) {
-            // 0 if no process is tracing.
-            return Buffer[i + LenTracerString] != '0';
-        }
+    const char *foundStr = std::strstr(Buffer, TracerString);
+
+    if (foundStr != nullptr) {
+        return foundStr[LenTracerString] != '0';
+    } else {
+        return false;
     }
-
-    return false;
 }
 
 }    // namespace pivot

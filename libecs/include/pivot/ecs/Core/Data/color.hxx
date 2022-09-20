819d8168845732c1ad3c4704bd7b8697ca4b89ac
Ersikan <julien.philippon@epitech.eu>
2022-09-20 22:45:25 +0200

    feat(ecs): Better default color

--- libecs/include/pivot/ecs/Core/Data/color.hxx
+++ libecs/include/pivot/ecs/Core/Data/color.hxx
@@ -7,7 +7,7 @@ namespace pivot::ecs::data
 /// Value containing a RGBA color
 struct Color {
     /// Color data in RGBA format
-    std::array<float, 4> rgba;
+    std::array<float, 4> rgba = {1, 1, 1, 1};
 
     /// Compare two color
     auto operator<=>(const Color &rhs) const = default;

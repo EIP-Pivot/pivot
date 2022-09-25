bc7512d784fbfae24c18a31893995a7f5a734159
Zacharie Corniere <zacharie@corniere.me>
2022-09-25 17:52:01 +0200

    fix(IResolver): fixed documentation error

--- libgraphics/include/pivot/graphics/interface/IResolver.hxx
+++ libgraphics/include/pivot/graphics/interface/IResolver.hxx
@@ -71,7 +71,7 @@ public:
     virtual bool prepareForDraw(const DrawSceneInformation &information) = 0;
 
     /// Bind extra ressources to the command buffer
-    virtual void bind([[maybe_unused]] vk::CommandBuffer &cmd) {}
+    virtual void bind(vk::CommandBuffer &) {}
 
 protected:
     /// Reference to the dispatcher that uses the current resolver

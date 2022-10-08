a0aba68123481b8ab09b8b00994fc2a9598b4046
Ersikan <julien.philippon@epitech.eu>
2022-10-08 14:28:17 +0200

    feat(pivot): Camera specific component array

---
+++ libpivot/include/pivot/internal/CameraArray.hxx
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <span>
+
+#include <pivot/ecs/Core/Component/DenseComponentArray.hxx>
+#include <pivot/ecs/Core/Component/array.hxx>
+#include <pivot/ecs/Core/Component/description_helpers.hxx>
+#include <pivot/ecs/Core/Component/error.hxx>
+#include <pivot/ecs/Core/types.hxx>
+
+#include <pivot/builtins/components/Camera.hxx>
+
+namespace pivot::internals
+{
+class CameraArray : public pivot::ecs::component::DenseTypedComponentArray<builtins::components::Camera>
+{
+public:
+    CameraArray(ecs::component::Description d): DenseTypedComponentArray<builtins::components::Camera>(d)
+    {
+        m_component_exist.push_back(true);
+        m_components.push_back(builtins::components::Camera{});
+        m_current_camera = 0;
+    }
+
+    std::optional<std::pair<Entity, builtins::components::Camera>> getCurrentCamera() const
+    {
+        if (m_current_camera < m_components.size() && m_component_exist.at(m_current_camera)) {
+            return {{m_current_camera, m_components[m_current_camera]}};
+        }
+
+        return std::nullopt;
+    }
+
+    void setCurrentCamera(Entity camera)
+    {
+        if (camera < m_components.size() && m_component_exist.at(camera)) { m_current_camera = camera; }
+    }
+
+private:
+    Entity m_current_camera;
+};
+}    // namespace pivot::internals

9fc5a9d883a38926d587e7c331a655ddc3247c1f
Ersikan <julien.philippon@epitech.eu>
2022-11-16 20:54:36 +0100

    feat: Build appimages

--- build_appimage.sh
+++ build_appimage.sh
@@ -5,8 +5,8 @@ set -e
 build_dir=$(realpath ${1:-build.appimage})
 echo Build directory $build_dir
 
-app_dir=$build_dir/AppDir
+app_dir=$(realpath AppDir)
 
-# cmake -B $build_dir -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+cmake -B $build_dir -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
 cmake --build $build_dir --parallel
 make -C $build_dir DESTDIR=$app_dir install
